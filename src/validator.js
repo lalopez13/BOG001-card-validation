83b09742da3727036e498bab590eeb26aae3c2cd
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-06-17 12:22:30 -0500

    v

--- src/validator.js
+++ src/validator.js
@@ -1,52 +1,49 @@
 const validator = {
-  maskify,
-  isValid
+  maskify: function maskify(creditCardNumber){
+    //console.log(creditCardNumber);
+      if(creditCardNumber.length>4){
+      var cutNumber = creditCardNumber.substr(0,creditCardNumber.length-4);
+      var changeString = cutNumber.replace(/[a-z0-9]/g, '#');
+      var lastNumber = creditCardNumber.substr(-4);
+      var fullNumber = changeString.concat(lastNumber);
+        return fullNumber;
+      }
+      else{
+        return creditCardNumber;
+      }
+    
+    },
+
+  isValid: function isValid(creditCardNumber){
+  
+    let array = [];
+    //convertir a array
+    let newArray = Array.from(creditCardNumber);
+    //console.log(newArray);
+    //Invertir Array
+    let reverseArray = newArray.reverse();
+    //console.log(reverseArray);
+    //recorre el array y busca los números en posición par
+    reverseArray.forEach((item, i) => {
+      if ( item, i % 2 !== 0) {
+        item = parseInt(item * 2);
+        //console.log(item);
+        if (item > 9) {
+          item = addDigits(item);
+          //console.log(item);
+        }
+      }
+      array.push(parseInt(item));
+      // Suma los numbers mayores que 10
+    });
+     return checkAlgorithm(array);
+    //console.log(checkAlgorithm(array));
+  }
 };
 
-export default validator;
-function maskify(creditCardNumber){
-  creditCardNumber.value;
-  //console.log(number)
-  if(creditCardNumber.value>4){
-    var cutNumber = number.substr(0,number.length-4);
-    var changeString = cutNumber.replace(/[a-z0-9]/g, '#');
-    var lastNumber = number.substr(-4);
-    var fullNumber = changeString.concat(lastNumber);
-    document.getElementById("numberCreditCard-mask").innerHTML = fullNumber;
-  }
-  else{
-    document.getElementById("numberCreditCard-mask").innerHTML = creditCardNumber.value;
-  }
-
-}
-//funcion validar
-let array = [];
-function isValid(creditCardNumber){
-  
-  //convertir a array
-  let newArray = Array.from(creditCardNumber.value);
-  //console.log(newArray);
-  //Invertir Array
-  let reverseArray = newArray.reverse();
-  //console.log(reverseArray);
-  //recorre el array y busca los números en posición par
-  reverseArray.forEach((item, i) => {
-    if ( item, i % 2 !== 0) {
-      item = parseInt(item * 2);
-      //console.log(item);
-      if (item > 9) {
-        item = addDigits(item);
-        //console.log(item);
-      }
-    }
-    let array.push( parseInt(item));
-    // Suma los numbers mayores que 10
-  });
-  checkAlgorithm(array);
-  console.log(checkAlgorithm(array));
-}
 
 // Suma los numbers mayores que 10
+
 function addDigits(item){
   let numstring = item.toString();
   //console.log (numstring);
@@ -54,29 +51,23 @@ function addDigits(item){
   let b = parseInt(numstring.charAt(1));
   item = a + b;
   //console.log(a+b);
-  return item;}
+  return item;
+}
 
   function checkAlgorithm(array) {
     let total = array.reduce((a, b) => a + b, 0);
     //console.log(total);
     if (total % 10 === 0) {
       //console.log(total);
-      pass();
+  
       return true;
 
 
     } else {
       //console.log(total);
-      document.getElementById("false").innerHTML = '*Su tarjeta no es válida';
-      var btn1 = document.getElementById("back");
-      var btn2 = document.getElementById("valid");
-      btn1.style.display = 'block';
-      btn2.style.display = 'none';
-
-
       return false;
-
       //console.log(total);
     }
-  };
-  
+  }
+
+  export default validator;
