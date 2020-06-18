80d7201e119631ab6cdb8a65d5c9951df7180b6a
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-06-17 19:39:57 -0500

    chore(init): Adds project files from curriculum

--- src/index.js
+++ src/index.js
@@ -3,34 +3,31 @@ import validator from "./validator.js";
 
 let creditCardNumber = document.getElementById("numberCreditCard");
 
-
 const element = document.querySelector("form");
-  element.addEventListener("submit", (event) => {
+element.addEventListener("submit", (event) => {
   event.preventDefault();
   verifiedSubmit();
- 
 });
 
-const donateButton = document.getElementsByClassName('donate-btn');
+const donateButton = document.getElementsByClassName("donate-btn");
 
 for (var i = 0; i < donateButton.length; i++) {
-  donateButton[i].addEventListener('click', show);
+  donateButton[i].addEventListener("click", show);
 }
 
-
-function verifiedSubmit (){
+function verifiedSubmit() {
   var valid = validator.isValid(creditCardNumber.value);
-  if (valid){
+  if (valid) {
     pass();
     var maskNumberValid = validator.maskify(creditCardNumber.value);
-    document.getElementById("creditCardNumberId-mask").innerHTML = maskNumberValid;
-
-  }else{
-      document.getElementById("false").innerHTML = '*Su tarjeta no es válida';
-      hideBtn();
+    document.getElementById(
+      "creditCardNumberId-mask"
+    ).innerHTML = maskNumberValid;
+  } else {
+    document.getElementById("false").innerHTML = "*Su tarjeta no es válida";
+    hideBtn();
   }
-  
-  }
+}
 
 function show() {
   var div1 = document.getElementById("aboutUs");
@@ -46,11 +43,10 @@ function pass() {
   var div2 = document.getElementById("pass");
   div1.style.display = "none";
   div2.style.display = "block";
- 
 }
-function hideBtn(){
+function hideBtn() {
   var btn1 = document.getElementById("back");
   var btn2 = document.getElementById("valid");
-  btn1.style.display = 'block';
-  btn2.style.display = 'none';
+  btn1.style.display = "block";
+  btn2.style.display = "none";
 }
