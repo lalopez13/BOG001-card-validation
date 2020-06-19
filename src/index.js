6a2dd4e788b44f779c37e19b5d206b602747d52e
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-06-19 10:48:54 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import validator from "./validator.js";
 //console.log(validator);
 
+//Numero de tarjeta de credito del usuario
 let creditCardNumber = document.getElementById("numberCreditCard");
 
+//Eventos del formulario
 const element = document.querySelector("form");
 element.addEventListener("submit", (event) => {
   event.preventDefault();
   verifiedSubmit();
 });
 
+//Evento del boton donar
 const donateButton = document.getElementsByClassName("donate-btn");
 
 for (var i = 0; i < donateButton.length; i++) {
   donateButton[i].addEventListener("click", show);
 }
 
+//Funcion donde se llama IsValid/Maskify
 function verifiedSubmit() {
   var valid = validator.isValid(creditCardNumber.value);
   if (valid) {
@@ -29,6 +33,7 @@ function verifiedSubmit() {
   }
 }
 
+//Mostrar y ocultar div
 function show() {
   var div1 = document.getElementById("aboutUs");
   var div2 = document.getElementById("select-donate");
@@ -38,12 +43,14 @@ function show() {
   div3.style.display = "block";
 }
 
+//Mostrar mensaje si la tarjeta es valida
 function pass() {
   var div1 = document.getElementById("form-box");
   var div2 = document.getElementById("pass");
   div1.style.display = "none";
   div2.style.display = "block";
 }
+//Mostrar y ocultar botones
 function hideBtn() {
   var btn1 = document.getElementById("back");
   var btn2 = document.getElementById("valid");
